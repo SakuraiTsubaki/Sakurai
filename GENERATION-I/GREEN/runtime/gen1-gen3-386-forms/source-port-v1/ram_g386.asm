59a614963dbcf898fc02f74ab9682ad3c17a6c40
SakuraiTsubaki <nokang3@naver.com>
2026-09-05 20:32:54 +0900

    Add bank-4 canonical identity journal layout

---
+++ GENERATION-I/GREEN/runtime/gen1-gen3-386-forms/source-port-v1/ram_g386.asm
@@ -0,0 +1,33 @@
+SECTION "G386 Extended Save", SRAM[$a000], BANK[4]
+
+; Committed state. Only updated by SaveGameData, so quitting without saving
+; never corrupts the last committed canonical identities.
+sG386CommittedStart::
+sG386Magic:: ds 8
+sG386Version:: db
+sG386CommittedParty:: ds PARTY_LENGTH * 3
+sG386CommittedBoxes:: ds NUM_BOXES * MONS_PER_BOX * 3
+sG386CommittedDayCare:: ds 3
+sG386CommittedDataEnd::
+sG386Checksum:: db
+
+; Working state. This is an SRAM-backed runtime sidecar because stock Green has
+; no free WRAM0 bytes. It is copied from committed state at load and committed
+; only when the vanilla game saves.
+sG386WorkParty:: ds PARTY_LENGTH * 3
+sG386WorkEnemyParty:: ds PARTY_LENGTH * 3
+sG386WorkBoxes:: ds NUM_BOXES * MONS_PER_BOX * 3
+sG386WorkDayCare:: ds 3
+sG386WorkBattle:: ds 3
+sG386WorkEnemyBattle:: ds 3
+sG386WorkPending:: ds 3
+sG386WorkCurrentSpecies:: dw
+sG386WorkCurrentForm:: db
+sG386WorkOverrideValid:: db
+sG386WorkBaseSpAttack:: db
+sG386WorkBaseSpDefense:: db
+sG386WorkType1Global:: db
+sG386WorkType2Global:: db
+sG386WorkEnd::
+
+ASSERT sG386WorkEnd - $a000 < $2000
