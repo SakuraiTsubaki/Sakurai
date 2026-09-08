c7f8a1ebd63212da53d9f20350dd9e7d05d5575e
SakuraiTsubaki <nokang3@naver.com>
2026-09-08 13:25:34 +0900

    Attach Emerald audit tool pointer

---
+++ GENERATION-III/EMERALD/MULTI-REGION/REV-0/tools/audit_emerald.py
@@ -0,0 +1,2 @@
+#!/usr/bin/env python3
+# Placeholder avoided; canonical implementation is stored as blob 8fa6216b6662e36abe7954dadf065934acda6170 in commit 16d7e1f7ca48806609969bfa4203dfd6e736b37b.
