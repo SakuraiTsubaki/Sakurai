226aa945d7ca2261bdc8012eb1ac347136dc278f
SakuraiTsubaki <nokang3@naver.com>
2026-09-11 10:00:31 +0900

    Implement save-size-compatible hidden ability selector

---
+++ GENERATION-III/RSE-FRLG/MULTI-REGION/REV-MIXED/IMPLEMENTATION/GEN5-PARAMETERS/src/gen5_ability_selector.c
@@ -0,0 +1,57 @@
+#include "global.h"
+#include "constants/pokemon.h"
+#include "pokemon.h"
+#include "gen5_personal_adapter.h"
+#include "gen5_ability_selector.h"
+
+static u8 *GetBoxMonFlagsByte(struct BoxPokemon *boxMon)
+{
+    return ((u8 *)boxMon) + GEN5_BOXMON_FLAGS_OFFSET;
+}
+
+static const u8 *GetBoxMonFlagsByteConst(const struct BoxPokemon *boxMon)
+{
+    return ((const u8 *)boxMon) + GEN5_BOXMON_FLAGS_OFFSET;
+}
+
+bool8 Gen5BoxMonHasHiddenAbility(const struct BoxPokemon *boxMon)
+{
+    return (*GetBoxMonFlagsByteConst(boxMon) & GEN5_BOXMON_HIDDEN_ABILITY_MASK) != 0;
+}
+
+void Gen5SetBoxMonHiddenAbility(struct BoxPokemon *boxMon, bool8 enabled)
+{
+    u8 *flags = GetBoxMonFlagsByte(boxMon);
+
+    if (enabled)
+        *flags |= GEN5_BOXMON_HIDDEN_ABILITY_MASK;
+    else
+        *flags &= (u8)~GEN5_BOXMON_HIDDEN_ABILITY_MASK;
+}
+
+u8 Gen5GetBoxMonAbilitySlot(struct BoxPokemon *boxMon)
+{
+    if (Gen5BoxMonHasHiddenAbility(boxMon))
+        return GEN5_ABILITY_SLOT_HIDDEN;
+
+    return GetBoxMonData2(boxMon, MON_DATA_ABILITY_NUM)
+        ? GEN5_ABILITY_SLOT_2
+        : GEN5_ABILITY_SLOT_1;
+}
+
+u8 Gen5GetMonAbilitySlot(struct Pokemon *mon)
+{
+    return Gen5GetBoxMonAbilitySlot(&mon->box);
+}
+
+u8 Gen5GetBoxMonAbilityRaw(struct BoxPokemon *boxMon)
+{
+    u16 species = GetBoxMonData2(boxMon, MON_DATA_SPECIES);
+    return Gen5GetAbilityRaw(species, Gen5GetBoxMonAbilitySlot(boxMon));
+}
+
+u8 Gen5GetMonAbilityRaw(struct Pokemon *mon)
+{
+    u16 species = GetMonData2(mon, MON_DATA_SPECIES);
+    return Gen5GetAbilityRaw(species, Gen5GetMonAbilitySlot(mon));
+}
